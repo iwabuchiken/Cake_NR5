83df734aeaf69351e7a4dc0013c0445d89185f2d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-10-15 12:32:50 +0900

    D-14/2
    v-1.0
    ---
    1. get info => some items of a single eq
    
        ==> done

--- app/Controller/EqsController.php
+++ app/Controller/EqsController.php
@@ -5,9 +5,106 @@ class EqsController extends AppController {
 
 	public function index() {
 		$this->set('eqs', $this->Eq->find('all'));
+		
+		$eqs = $this->_index__Get_EqInfo();
+		
 	}
+
+	public function
+	_index__Get_EqInfo() {
+		
+		$url = "http://typhoon.yahoo.co.jp/weather/jp/earthquake/list/";
+		
+		//REF http://sourceforge.net/projects/simplehtmldom/files/simplehtmldom/1.5/
+		$html = file_get_html($url);
+		
+		$trs = $html->find('table tr');
+		
+		$eqs = $this->_index__Conv_Html_to_Eqs($trs);
+		
+// 		$table = $html->find('table');
+		
+// 		if ($table != null) {
+			
+// 			debug(count($table));	//=> 1
+
+// 			$trs = $html->find('table tr');
+			
+// 			debug(count($trs));	//=> 101
+			
+// 			debug(get_class($trs[0]));	//=> simple_html_dom_node
+			
+// 			$trs_0_children = $trs[0]->children();
+			
+// 			debug(count($trs_0_children));	//=> 5
+			
+// 			//td
+// 			$td_1 = $trs_0_children[0];	// [node, node,...]
+
+// 			debug(get_class($td_1));	//=> simple_html_dom_node
+			
+// 			debug($td_1->plaintext);	//=> '  発生時刻     '
+			
+// // 			$trs_children = $trs->children();	//=> Call to a member function children() on a non-object
+			
+// // 			$tds_1 = $trs[0]->children();
+			
+// // 			debug(get_class($tds_1[0]));	//=> simple_html_dom_node
+			
+// // 			$td_1 = $trs->first_child();	//=> Call to a member function first_child() on a non-object	
+			
+// // 			$td_1 = $trs[0]['td:first'];	//=> Cannot use object of type simple_html_dom_node as array
+			
+// // 			debug(array_keys($trs[0]));
+// // 			debug($trs[0]);
+// // 			$td = $trs->children(1);	//=> Call to a member function children() on a non-object
+// // 			debug(array_keys($table));
+// // 			debug(array_keys($table[0]));
+// // 			debug($table[0]);
+			
+// // 			$trs = $table->tr;
+			
+// // 			debug(count($trs));
+			
+// 		} else {
+			
+// 			debug("\$table => null");
+			
+// 		}
+		
+// 		debug(get_class($table));
+// 		debug($table);	//=> Allowed memory size of 134217728 bytes exhausted
+		
+	}//_index__Get_EqInfo
 	
-	public function view($id = null) {
+	public function 
+	_index__Conv_Html_to_Eqs($trs) {
+		
+		$eqs = array();
+		
+		$tr_1 = $trs[1];
+		
+		$tds_1 = $tr_1->children();
+		
+		$eq = $this->Eq->create();
+		
+		$eq['time_eq'] = $tds_1[0]->plaintext;
+		$eq['time_pub'] = $tds_1[1]->plaintext;
+		$eq['epi'] = $tds_1[2]->plaintext;
+		
+		debug($eq);
+		
+// 		$td_1 = $tds_1[0];
+		
+// 		$time_eq = $td_1->plaintext;
+		
+// 		debug($time_eq);	//=> '2014年10月15日 7時52分ごろ'
+// 		debug(count($tds_1));	//=> 5
+		
+	}//_index__Conv_Html_to_Eqs
+	
+	public function 
+	view($id = null) {
 		if (!$id) {
 			throw new NotFoundException(__('Invalid eq'));
 		}
