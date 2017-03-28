1d5bca8452f5aaae257380d54ea272ea6b39c4b2
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-03-28 16:49:34 +0900

    D-33
    1#1/steps=~#2.5
    ---
    1. TEST-1: "function test_1_1_1_get_html_content()"
    
        ==> done
    
    ----------------- TEMPLATE
    1.  --> .
    
        ==> done

--- app/Controller/Articles2Controller.php
+++ app/Controller/Articles2Controller.php
@@ -8,7 +8,94 @@ class Articles2Controller extends AppController {
 	public function 
 	index() {
 
+		$this->test_1_1_1_get_html_content();
+		
 		
 	}//index()
+
+	function test_1_1_1_get_html_content() {
+
+		/******************** (20 '*'s)
+		 *
+		 * get: url content
+		 *
+		 * ref: app\Controller\ArticlesController.php\__index_Get_Articles__Top
+		 *
+		 ********************/
+		$name_genre = "tech_science";
+		
+		$url = "http://www.asahi.com/".$name_genre."/list/";
+		// 		$url = "http://headlines.yahoo.co.jp/hl?c=$genre&t=l";
+		
+		//REF http://sourceforge.net/projects/simplehtmldom/files/simplehtmldom/1.5/
+		$html = file_get_html($url);
+		
+		// hrefs
+		$ahrefs = $html->find('a[href]');
+		
+		debug("\$ahrefs => ".count($ahrefs));
+		
+		// validate
+		if (count($ahrefs) < 1) {
+			
+			debug("\$ahrefs => less than 1");
+			
+			return;
+			
+		}
+		
+// 		//debug	//=> Error: Allowed memory size of 134217728 bytes exhausted (tried to allocate 73330 bytes)	
+// 		if (count($ahrefs) > 0) {
+// // 		if (count($ahrefs) > 5) {
+		
+		//ref http://php.net/manual/ja/function.array-slice.php
+// // 			debug(array_slice($ahrefs, 0, 3));
+// 			debug($ahrefs[0]);
+	
+		
+// 		} else {
+		
+// 			debug("\$ahrefs => less than 1");
+			
+// 		}//if (count($ahrefs) > 5)
+
+		/******************** (20 '*'s)
+		*
+		* show href value
+		*
+		********************/
+		$count = 0;
+		$max = 5;
+
+		foreach ($ahrefs as $ahref) {
+			
+			debug("\$ahref->href => $ahref->href");
+			debug("\$ahref->plaintext => $ahref->plaintext"." (".mb_strlen($ahref->plaintext).") chars");
+// 			debug("\$ahref->plaintext => $ahref->plaintext"." (".count($ahref->plaintext).") chars");
+// 			debug("\$ahref => $ahref");
+			
+// 			debug("get_class(\$ahref->plaintext) => ".get_class($ahref->plaintext));
+			
+			$count += 1;
+			
+			if ($count > $max) {
+				
+				break;
+				
+			}
+		
+// 			if (Utils::startsWith($ahref->href, "http://headlines")
+// 					&& count(explode("-", $ahref->href)) > 3) {
+		
+// 						array_push($ahrefs_hl, $ahref);
+		
+// 					}
+		
+		}//foreach ($ahrefs as $ahref)
+
+		
+		
+		
+	}//test_1_1_1_get_html_content()
 	
 }//class ArticlesController extends AppController
