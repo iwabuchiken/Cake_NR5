2502d30d8922f58b9686542bc8f96e6d304ab75e
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-03-28 16:19:13 +0900

    D-33
    1#1/steps=#1.1)
    ---
    1. controller: "Articles2Controller.php" (signature only)
    
        ==> done
    
    ----------------- TEMPLATE
    1.  --> .
    
        ==> done

---
+++ app/Controller/Articles2Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+class Articles2Controller extends AppController {
+// class ArticlesController extends AppController {
+	public $helpers = array('Html', 'Form', 'Mytest');
+// 	public $helpers = array('Html', 'Form');
+
+	public function 
+	index() {
+
+		
+	}//index()
+	
+}//class ArticlesController extends AppController
